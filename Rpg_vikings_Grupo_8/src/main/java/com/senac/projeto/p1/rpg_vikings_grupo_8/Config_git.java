92894e77a9d53fddf8757597466138db5d3b9614
genivaldo.aanjos <gda_anjos@hotmail.com.br>
2022-04-25 22:15:43 -0300

    version 1.0.0

---
+++ Rpg_vikings_Grupo_8/src/main/java/com/senac/projeto/p1/rpg_vikings_grupo_8/Config_git.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.senac.projeto.p1.rpg_vikings_grupo_8;
+
+/**
+ *
+ * @author genivaldo.aanjos
+ */
+public class Config_git {
+    
+    public static void main(String[] args) {
+        
+        int soma = 1+1;
+        
+        System.out.println("O reusultadoda soma é "+ soma);
+    }
+    
+}
